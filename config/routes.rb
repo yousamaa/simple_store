6cd693b2b9c91bba0eab8ca38b2ad83b1677b920
yousamaaa <usama.yousaf@devsinc.com>
2022-08-01 23:00:57 +0500

    Added root toute to the products#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'products#index'
+
   get 'products', to: 'products#index', as: 'products'
 # ^verb  ^URI          ^controller ^action  ^refer: products_path
 
