661ad95739d64a15d580d380ce5bc5e18dbf44a4
yousamaaa <usama.yousaf@devsinc.com>
2022-08-01 21:57:32 +0500

    fixed routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def index
   end
+  # After this action the associated view will
+  # be automatically loaded and any instance
+  # variable will be shared with this view.
+  # app/views/products/index.html.erb
 
   def show
   end
+  # app/views/products/show.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  get 'products', to: 'products#index', as: 'products'
+# ^verb  ^URI          ^controller ^action  ^refer: products_path
+
+  get 'products/:id', to: 'products#show', as: 'product'
+# ^verb  ^URI          ^controller ^action      ^refer: product_path  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
