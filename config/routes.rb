ffe6c35f48ae8e23f319591f1a0144dd88a5d72e
yousamaaa <usama.yousaf@devsinc.com>
2022-08-01 23:09:27 +0500

    Replaced our custom index and show routes with resources based routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'products#index'
 
-  get 'products', to: 'products#index', as: 'products'
+  # Generate the 7 default RESTful routes.
+  resources :products, only: [:index, :show]
+
+#  get 'products', to: 'products#index', as: 'products'
 # ^verb  ^URI          ^controller ^action  ^refer: products_path
 
-  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+#  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
 # ^verb  ^URI          ^controller ^action      ^refer: product_path  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
